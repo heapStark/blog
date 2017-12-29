0e7bd033d47b51d3470aae09849b831359c08b25
wangzhilei4 <wangzhilei3@jd.com>
2017-12-29 18:19:18 +0800

    并发

--- src/main/java/heapStark/blogCode/concurrent/Main.java
+++ src/main/java/heapStark/blogCode/concurrent/Main.java
@@ -24,17 +24,18 @@ public class Main {
 
         SafeCounter counter = new SafeCounter();
         //启动五个线程开始计数
-        CountDownLatch countDownLatch = new CountDownLatch(5);
+        //CountDownLatch countDownLatch = new CountDownLatch(5);
         Semaphore semaphore = new Semaphore(1);
         semaphore.acquire();
         //semaphore.acquire();
         boolean b = semaphore.tryAcquire();
+        assert (!b);
         //semaphore.release();
         CyclicBarrier cyclicBarrier = new CyclicBarrier(6,
                 () -> {
                     try {
                         TimeUnit.SECONDS.sleep(5);
-                    } catch (InterruptedException e) {
+                    } catch (InterruptedException ignored) {
 
                     }
                     System.out.println(Thread.currentThread().getName() + ": " + "action");
@@ -43,14 +44,15 @@ public class Main {
             for (int i = 0; i < 1; i++) {
                 System.out.println(Thread.currentThread().getName() + ": " + "before");
                 try {
+                    counter.counter();
                     cyclicBarrier.await();
-                } catch (Exception e) {
+                } catch (Exception ignored) {
 
                 }
                 System.out.println(Thread.currentThread().getName() + ": " + "after");
                 try {
                     TimeUnit.SECONDS.sleep(10);
-                } catch (InterruptedException e) {
+                } catch (InterruptedException ignored) {
 
                 }
             }
@@ -62,13 +64,13 @@ public class Main {
                 System.out.println(Thread.currentThread().getName() + ": " + "reuse before");
                 try {
                     cyclicBarrier.await();
-                } catch (Exception e) {
+                } catch (Exception ignored) {
 
                 }
                 System.out.println(Thread.currentThread().getName() + ": " + "reuse after");
                 try {
                     TimeUnit.SECONDS.sleep(10);
-                } catch (InterruptedException e) {
+                } catch (InterruptedException ignored) {
 
                 }
             }
@@ -88,9 +90,7 @@ public class Main {
         Future<Integer> future = pool.submit(sumTask);
         try {
             assert (future.get() == 55);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
     }
@@ -102,19 +102,19 @@ public class Main {
         assert (executorService.isTerminated() == false);
         Future<List<String>> future = executorService.submit(new Call());
         try {
-            assert (future.isDone() == false);
+            assert (!future.isDone() );
             List<String> result = (List) future.get();
             //executorService.shutdownNow();//调用future.get()抛出异常
             //future.cancel(false);
             assert (result.equals(Arrays.asList("hello", "world")));
 
         } catch (InterruptedException e) {
-            assert (false);
+            assert false;
         } catch (Exception e) {
-            assert (false);
+            assert false;
         }
-        assert (executorService.isShutdown() == false);
-        assert (executorService.isTerminated() == false);
+        if (executorService.isShutdown() != false) throw new AssertionError();
+        assert (!executorService.isTerminated() );
 
     }
 
@@ -155,9 +155,7 @@ public class Main {
 
             assert (singleFuture.get() == "hello");
             assert ((Long) callFuture.get() <= 20 && (Long) callFuture.get() >= 9);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
         Map<String, Long> map = new HashMap<>();
@@ -170,7 +168,7 @@ public class Main {
             map.put("key", new Date().getTime());
         }, 1000, 1000, TimeUnit.MILLISECONDS);
 
-        assert (future.isDone() == false);
+        assert (!future.isDone());
 
         try {
             countDownLatch.await();
