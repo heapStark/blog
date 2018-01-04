ae6a4374a963374eb352d4da884833ddfac84137
wangzhilei4 <wangzhilei3@jd.com>
2018-01-04 12:59:15 +0800

    redisson

--- src/main/java/heapStark/blogCode/redis/jedis/JedisTest.java
+++ src/main/java/heapStark/blogCode/redis/jedis/JedisTest.java
@@ -1,31 +1,34 @@
 package heapStark.blogCode.redis.jedis;
 
 import org.junit.Test;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 
-import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * blogcode
  * Created by wangzhilei3 on 2018/1/3.
  */
 public class JedisTest {
+    private final static String KEY = "hello";
 
-    private JedisPool jedisPool = JedisClientPool.getPool();
+    @Test
+    public void testLock() {
+        RedisCache redisCache = new RedisCache();
+        redisCache.delateKey(KEY);
+        redisCache.getLock(KEY);
+        Long pttl = redisCache.pttlKey(KEY);
+        assert (pttl <= 60000);
 
-    private Jedis getJedis() {
-        Jedis jedis = null;
         try {
-            jedis = jedisPool.getResource();
-        } catch (Exception e) {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return jedis;
-    }
-    @Test
-    public void jedis (){
-        Jedis jedis = getJedis();
-
+        redisCache.getLock(KEY);
+        System.out.println(redisCache.pttlKey(KEY));
+        Long pttl2 = redisCache.pttlKey(KEY);
+        System.out.println(pttl2);
+        assert (50000 < pttl2 && pttl2 <= 60000);
+        redisCache.delateKey(KEY);
     }
 }
