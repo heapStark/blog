d934e90bfa375c4cf23241b244d538769e2f8c62
heapStark <17611503218>
2018-01-28 23:03:41 +0800

    List

--- src/main/java/heapStark/blogCode/algorithm/Algorithm.java
+++ src/main/java/heapStark/blogCode/algorithm/Algorithm.java
@@ -230,11 +230,12 @@ public class Algorithm {
 
     /**
      * swapPairs pre, cur, temp 三组变量
+     *
      * @param head
      * @return
      */
     public static ListNode swapPairs(ListNode head) {
-        if(head == null || head.next == null) return head;
+        if (head == null || head.next == null) return head;
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         for (ListNode pre = dummy, cur = head, temp; cur != null && cur.next != null; pre = cur, cur = cur.next) {
@@ -248,6 +249,7 @@ public class Algorithm {
 
     /**
      * 倒数k个前置
+     *
      * @param head
      * @param k
      * @return
@@ -273,7 +275,115 @@ public class Algorithm {
     }
 
     /**
+     * 链表去重
+     *
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode cur = head;
+        while (cur != null) {
+            while (cur.next != null && cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+
+    /**
+     * @param head
+     * @return
+     */
+    public ListNode deleteAllDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode newHead = new ListNode(head.val - 1);
+        newHead.next = head;
+        ListNode cur = head;
+        ListNode pre = newHead;
+        while (cur != null && cur.next != null) {
+            if (cur.val != cur.next.val) {
+                pre = cur;
+            } else {
+                while (cur.next != null && cur.val == cur.next.val) {
+                    cur = cur.next;
+                }
+                pre.next = cur.next;
+            }
+            cur = cur.next;
+        }
+        return newHead.next;
+    }
+
+    /**
+     * sortedListToBST
+     * @param head
+     * @return
+     */
+    public TreeNode sortedListToBST(ListNode head)
+    {
+        if(head==null) return null;
+        if(head.next == null) return new TreeNode(head.val);
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        TreeNode root = new TreeNode(slow.next.val);
+        root.right = sortedListToBST(slow.next.next);
+        slow.next = null;
+        root.left = sortedListToBST(head);
+        return root;
+    }
+
+    /**
+     *
+     1) begin开始指向0， end一直后移，直到begin - end区间包含T中所有字符。
+     记录窗口长度d
+     2) 然后begin开始后移移除元素，直到移除的字符是T中的字符则停止，此时T中有一个字符没被
+     包含在窗口，
+     3) 继续后移end，直到T中的所有字符被包含在窗口，重新记录最小的窗口d。
+     4) 如此循环知道end到S中的最后一个字符。
+     时间复杂度为O(n)
+     * @param S
+     * @param T
+     * @return
+     */
+    public String minWindow(String S, String T) {
+        int[] map = new int[128];
+        //init map, 记录T中每个元素出现的次数
+        for(int i = 0; i < T.length(); i++) {
+            map[T.charAt(i)]++;
+        }
+        // begin end两个指针指向窗口的首位，d记录窗口的长度， counter记录T中还有几个字符没被窗口包含
+        int begin = 0, end = 0, d = Integer.MAX_VALUE, counter = T.length(), head = 0;
+        // end指针一直向后遍历
+        while(end < S.length()) {
+            // map[] > 0 说明该字符在T中出现，counter-- 表示对应的字符被包含在了窗口，counter--, 如果s中的字符没有在T中出现，则map[]中对应的字符-1后变为负值
+            if(map[S.charAt(end++)]-- > 0) {
+                counter--;
+            }
+            // 当counter==0时，说明窗口已经包含了T中的所有字符
+            while (counter == 0) {
+                if(end - begin < d) {
+                    d = end - (head = begin);
+                }
+                if(map[S.charAt(begin++)]++ == 0) {  // begin开始后移，继续向后寻找。如果begin后移后指向的字符在map中==0，表示是在T中出现的，如果没有出现，map[]中的值会是负值。
+                    counter++;                      // 在T中的某个字符从窗口中移除，所以counter++。
+                }
+            }
+        }
+        return d==Integer.MAX_VALUE ? "" :S.substring(head, head+d);
+    }
+
+
+    /**
      * 链表求和
+     *
      * @param l1
      * @param l2
      * @return
@@ -317,27 +427,7 @@ public class Algorithm {
         System.out.print("");
     }
 
-    /**
-     * 有序列表转二叉搜索树
-     *
-     * @param head
-     * @return
-     */
-    public TreeNode sortedListToBST(ListNode head) {
-        if (head == null) return null;
-        if (head.next == null) return new TreeNode(head.val);
-        ListNode slow = head;
-        ListNode fast = head.next;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        TreeNode root = new TreeNode(slow.next.val);
-        root.right = sortedListToBST(slow.next.next);
-        slow.next = null;
-        root.left = sortedListToBST(head);
-        return root;
-    }
+
 
     /**
      * 快排
