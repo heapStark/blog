d7ad21a34f6ee2f073c0d0f6ea2343d9490cbb9f
heapStark <17611503218>
2018-01-28 22:39:57 +0800

    List

--- src/main/java/heapStark/blogCode/algorithm/Algorithm.java
+++ src/main/java/heapStark/blogCode/algorithm/Algorithm.java
@@ -247,6 +247,32 @@ public class Algorithm {
     }
 
     /**
+     * 倒数k个前置
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k == 0 || head == null || head.next == null)
+            return head;
+        ListNode preHead = new ListNode(0);
+        preHead.next = head;
+        ListNode cur = head;
+        ListNode pre = head;
+        int total;
+        for (total = 1; cur.next != null; total++)
+            cur = cur.next;
+        for (int i = 1; i < total - k % total; i++) {
+            pre = pre.next;
+        }
+        cur.next = preHead.next;
+        preHead.next = pre.next;
+        pre.next = null;
+
+        return preHead.next;
+    }
+
+    /**
      * 链表求和
      * @param l1
      * @param l2
