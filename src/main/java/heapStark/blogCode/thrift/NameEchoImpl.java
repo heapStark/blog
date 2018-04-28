e4327dae376222f90ca6c3c24530cccd69823e50
wangzhilei <wangzhilei@xiaomi.com>
2018-04-28 14:57:37 +0800

    good

--- src/main/java/heapStark/blogCode/thrift/NameEchoImpl.java
+++ src/main/java/heapStark/blogCode/thrift/NameEchoImpl.java
@@ -1,4 +1,10 @@
 package heapStark.blogCode.thrift;
+import heapStark.blogCode.thriftDemo.service.NameService;
+import org.apache.thrift.TException;
 
-public class NameEchoImpl {
+public class NameEchoImpl implements NameService.Iface{
+    @Override
+    public String echoName(String name) throws TException {
+        return name;
+    }
 }
